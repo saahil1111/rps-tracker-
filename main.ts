0d42d0c1bf3a886362bfebf5246387f18bff4ccf
saahil1111 <122548152+saahil1111@users.noreply.github.com>
2023-01-30 11:16:10 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+function showscoreboard () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player A;" + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player B;" + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds" + Rounds)
+}
+let Ties = 0
+let Rounds = 0
+let PB = 0
+let PA = 0
+OLED.init(128, 64)
+PA = 0
+PB = 0
+Rounds = 0
+Ties = 0
+OLED.writeStringNewLine("Shall we play a game?")
+basic.pause(2000)
+showscoreboard()
